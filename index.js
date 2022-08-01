27fc493bbe55850f82396919d6dfa4db418b0550
Tubeee <alexander011@live.se>
2022-08-01 22:41:05 +0200

    first working prototype after working out a few kinks...

--- index.js
+++ index.js
@@ -6,20 +6,11 @@ const CLIENT_CODECS = [{
     type: "audio",
     name: "opus",
     priority: 1000,
-    payloadType: 120
+    payload_type: 120
 }]
 
-VoiceEngine.getSupportedVideoCodecs(videoCodecs => {
-    CLIENT_CODECS.concat(videoCodecs.map((codec, i) => ({
-            type: "video",
-            name: codec,
-            priority: 1000*(i+1),
-            payloadType: 101 + 2*i,
-            rtxPayloadType: 101 + 2*i + 1
-        })
-    ))
-})
-
+VoiceEngine.setInputDevice('default')
+VoiceEngine.setOutputDevice('default')
 
 const ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=9')
 ws.on('message', onMessage)
@@ -29,31 +20,32 @@ function init(token) { // [1]
         {
             op: 2,
             d: {
-              token: "",
-              capabilities: 509,
-              properties: {
-                os: "Linux",
-                browser: "Discord Client",
-                release_channel: "stable",
-                client_version: "0.0.18",
-                os_version: "5.18.13-200.fc36.x86_64",
-                os_arch: "x64",
-                system_locale: "en-US",
-                window_manager: "GNOME,gnome-xorg",
-                client_build_number: 138734,
-                client_event_source: null
-              },
-              presence: {status: "invisible", since: 0, activities: [], afk: false},
-              compress: false,
-              client_state: { guild_hashes: {}, highest_last_message_id: "0", read_state_version: 0, user_guild_settings_version: -1 },
+                token,
+                capabilities: 509,
+                properties: {
+                    os: "Linux",
+                    browser: "Discord Client",
+                    release_channel: "stable",
+                    client_version: "0.0.18",
+                    os_version: "5.18.13-200.fc36.x86_64",
+                    os_arch: "x64",
+                    system_locale: "en-US",
+                    window_manager: "GNOME,gnome-xorg",
+                    client_build_number: 138734,
+                    client_event_source: null
+                },
+                presence: { status: "online", since: 0, activities: [], afk: false },
+                compress: false,
+                client_state: { guild_hashes: {}, highest_last_message_id: "0", read_state_version: 0, user_guild_settings_version: -1 },
             }
-          }
+        }
     ))
 }
 
 
 let seq = 0
 function onMessage(msg) {
+    //debugger
     msg = JSON.parse(msg)
     switch (msg.op) {
         case 0:
@@ -62,17 +54,15 @@ function onMessage(msg) {
         case 9: // Invalid Session
             break
         case 10: // Hello
-            setInterval(()=>ws.send(JSON.stringify({op: 1, d: seq})) , msg.heartbeat_interval)
+            setInterval(() => ws.send(JSON.stringify({ op: 1, d: seq })), msg.d.heartbeat_interval)
         case 11: // Heartbeat ACK
     }
 }
 
-let voiceState
-let voiceGateway
-let userId
+let voiceState, voiceGateway, userId
 function onEvent(msg) {
     seq = msg.s
-    
+
     switch (msg.t) {
         case 'READY': //[2]
             userId = msg.d.user.id
@@ -81,50 +71,68 @@ function onEvent(msg) {
             if (msg.d.user_id == userId) voiceState = msg.d
             break
         case 'VOICE_SERVER_UPDATE': // [5]
-            const {endpoint, token, guild_id, channel_id} = msg.d
-            voiceGateway = new WebSocket(endpoint)
+            const { endpoint, token, guild_id, channel_id } = msg.d
+            voiceGateway = new WebSocket('wss://' + endpoint + '?v=7')
+            voiceGateway.on('message', msg => console.log('VoiceGateway recv', JSON.parse(msg)))
             voiceGateway.on('message', onVoiceMessage)
-            voiceGateway.send(JSON.stringify({
+            voiceGateway.on('open', e => voiceGateway.send(JSON.stringify({
                 op: 0,
                 d: {
                     server_id: guild_id || channel_id,
                     session_id: voiceState.session_id,
-                    streams: [{type: "video", rid: "100", qualiy: 100},{type: "video", rid: "50", qualiy: 50}],
+                    streams: [{ type: "video", rid: "100", qualiy: 100 }, { type: "video", rid: "50", qualiy: 50 }],
                     token,
                     user_id: voiceState.user_id,
                     video: true
                 }
             }))
+            )
     }
 }
 
 
-let voiceConnectionInfo
 let voiceInstance
 function onVoiceMessage(msg) {
     msg = JSON.parse(msg)
     switch (msg.op) {
         case 2: // Ready [7]
-            voiceConnectionInfo = msg.d
-            voiceInstance = VoiceEngine.createVoiceConnectionWithOptions(voiceState.user_id, voiceConnectionInfo, createVoiceConnectionCallback)
+            const { ip: address, port, ssrc, modes, experiments, streams: streamParameters } = msg.d
+            voiceInstance = VoiceEngine.createVoiceConnectionWithOptions(voiceState.user_id, { address, port, ssrc, modes, experiments: experiments.concat(['connection_log']), streamParameters, qosEnabled: false }, createVoiceConnectionCallback)
+            voiceInstance.setTransportOptions({
+                "inputMode": 1,
+                "inputModeOptions": {
+                    "vadThreshold": -60,
+                    "vadAutoThreshold": 3,
+                    "vadUseKrisp": false,
+                    "vadLeading": 5,
+                    "vadTrailing": 25
+                }
+            })
+            voiceInstance.setOnSpeakingCallback((_userId, speaking) => {
+                if (_userId == userId)
+                    voiceGateway.send(JSON.stringify({ op: 5, d: { ssrc, speaking, delay: 0 } }))
+            })
+
             break
         case 4: //Session Description [9]
-            const {mode, secret_key} = msg.d
-            voiceInstance.setEncryptionMode({encryptionSettings: {mode, secret_key}})
+            const { mode, secret_key: secretKey } = msg.d
+            voiceInstance.setTransportOptions({ encryptionSettings: { mode, secretKey } })
             break
         case 8: // Hello [6]
-            setInterval(()=>voiceGateway.send(JSON.stringify({op: 3, d: Date.now()})) , msg.heartbeat_interval)
+            setInterval(() => voiceGateway.send(JSON.stringify({ op: 3, d: Date.now() })), msg.d.heartbeat_interval)
             break
+        case 5: // Speaking //This seems to only be sent once per speaker when using the native client.
+            voiceInstance.mergeUsers([{ id: msg.d.user_id, ssrc: msg.d.ssrc, videoSsrc: 0, rtxSsrc: 0, mute: false, volume: 1 }])
     }
 
 }
 
-function createVoiceConnectionCallback(err, {protocol, address, port}) { // [8]
+function createVoiceConnectionCallback(err, { protocol, address, port }) { // [8]
     if (err) {
-        console.log(err)
+        console.log('createVoiceConnection error', err)
     } else {
         console.log('Connected')
-        voiceGateway.send({
+        voiceGateway.send(JSON.stringify({
             op: 1,
             d: {
                 protocol,
@@ -139,7 +147,7 @@ function createVoiceConnectionCallback(err, {protocol, address, port}) { // [8]
                 },
                 codecs: CLIENT_CODECS
             }
-        })
+        }))
     }
 }
 
@@ -157,3 +165,16 @@ function connectVoice(guildId, channelId) { // [3]
         }
     }))
 }
+
+
+WebSocket.prototype._send = WebSocket.prototype.send
+WebSocket.prototype.send = function (data) {
+    console.log('WS send', JSON.parse(data))
+    WebSocket.prototype._send.call(this, data)
+}
+
+ws.on('message', msg => console.debug('RGateway recv', JSON.parse(msg)))
+global.init = init
+global.connectVoice = connectVoice
+global.voiceInstance = voiceInstance
+global.VoiceEngine = VoiceEngine
---
+++ readme.md
@@ -0,0 +1,4 @@
+# status
+receiving and sending audio works
+# usage
+no real interface implemnted yet, test with `node --inspect .`, then `init(token)` and `connectVoice(guild_id, channel_id)` in the debugger console
